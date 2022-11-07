886d96ee45e535ca27e8266a8300bafd1fbfa21b
Vadim2k <44468431+VadimElFamoso@users.noreply.github.com>
2022-11-07 10:14:32 +0100

    Add files via upload

--- LightMode-DarkMode/app.js
+++ LightMode-DarkMode/app.js
@@ -6,16 +6,23 @@ const icone = document.querySelector('i');
 const circle = document.getElementById('circle');
 
 container.addEventListener('click', function(){
+    console.log('Le container est cliqué !');
+    //La couleur est Light, on va passer en Dark.
     if(title.innerText === 'Light-Mode'){
-        title.innerText = 'Dark-mode';
+        console.log('Première condition');
+        title.innerText = 'Dark-Mode';
+        title.style.color = 'white';
         body.classList.toggle('dark-mode');
-        icone.style.color = 'black';
+        icone.classList.toggle('fa-moon');
         circle.classList.toggle('circle-animation');
     }
+    //La couleur est Dark, on va passer en Light.
     else{
+        console.log('Deuxième condition');
         title.innerText = 'Light-Mode';
+        title.style.color = 'black';
         body.classList.toggle('dark-mode');
-        icone.style.color = 'white';
+        icone.classList.toggle('fa-sun');
         circle.classList.toggle('circle-animation');
     }
 
