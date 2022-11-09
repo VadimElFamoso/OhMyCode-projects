4852f06aea96b4b8295fce4273aac55a0f7bdbce
Vadim2k <44468431+VadimElFamoso@users.noreply.github.com>
2022-11-09 13:24:04 +0100

    Add files via upload

--- LightMode-DarkMode/app.js
+++ LightMode-DarkMode/app.js
@@ -3,27 +3,26 @@ const body = document.querySelector('body');
 const title = document.getElementById('title');
 const container = document.getElementById('container');
 const icone = document.querySelector('i');
-const circle = document.getElementById('circle');
+const circle = document.getElementById('circle')
+
 
 container.addEventListener('click', function(){
-    console.log('Le container est cliqué !');
+    
+    body.classList.toggle('dark-mode');
+    icone.classList.toggle('fa-moon');
+    circle.classList.toggle('circle-animation');
+    
+    
     //La couleur est Light, on va passer en Dark.
     if(title.innerText === 'Light-Mode'){
-        console.log('Première condition');
         title.innerText = 'Dark-Mode';
         title.style.color = 'white';
-        body.classList.toggle('dark-mode');
-        icone.classList.toggle('fa-moon');
-        circle.classList.toggle('circle-animation');
+        container.style.backgroundColor = '#93de87';
     }
     //La couleur est Dark, on va passer en Light.
     else{
-        console.log('Deuxième condition');
-        title.innerText = 'Light-Mode';
+        title.innerText = 'Light-Mode'
         title.style.color = 'black';
-        body.classList.toggle('dark-mode');
-        icone.classList.toggle('fa-sun');
-        circle.classList.toggle('circle-animation');
+        container.style.backgroundColor = '#333030';
     }
-
 });
