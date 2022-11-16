d9574dea5119e9de27709c6b66bea9a6b39b7f71
Vadim2k <44468431+VadimElFamoso@users.noreply.github.com>
2022-11-16 09:54:00 +0100

    Commit du 16.11.22

--- Menu Burger/app.js
+++ Menu Burger/app.js
@@ -1,8 +1,12 @@
 const modal = document.querySelector('#navbar-mobile #modal');
+console.log(modal);
 const icone = document.querySelector('#navbar-mobile i');
+const title = document.querySelector('#navbar-mobile h2');
 
 icone.addEventListener('click', function(){
     console.log('oui');
     modal.classList.toggle('change-modal');
     icone.classList.toggle('fa-times');
+    title.classList.toggle('change-text');
+
 })
